125b3a1cb71966a00c234ad32b795cb5726dbd62
dslm65 <dslm65@gmail.com>
2017-06-11 12:20:48 +0530

    uploading 3 projects iDeliverMobile, Locatapp, SamplePRO

---
+++ SamplesPRO/Chapter4/iDeliverMobile/iDeliverMobile/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iDeliverMobile
+//
+//  Created by Kevin McNeish on 8/30/12.
+//  Copyright (c) 2012 Oak Leaf Enterprises, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
